8463a9aee3eece8dedb851c13fe3ffa840192f53
Omar Reda <omarredaelsayedmohamed@gmail.com>
2022-06-27 14:02:17 -0700

    removed token from api

--- routes/packagesRoute.js
+++ routes/packagesRoute.js
@@ -3,12 +3,12 @@ const packagesController = require('../controllers/packagesController')
 const { verifyToken, verifyAdmin } = require('../middlewares/verifyToken')
 
 
-router.get('/packages', verifyToken, (request, response) => packagesController.getPackages(request, response))
+router.get('/packages', (request, response) => packagesController.getPackages(request, response))
 
-router.put('/packages/:packageId', verifyAdmin, (request, response) => packagesController.updatePackage(request, response))
+router.put('/packages/:packageId', (request, response) => packagesController.updatePackage(request, response))
 
-router.post('/packages', verifyAdmin, (request, response) => packagesController.addPackage(request, response))
+router.post('/packages', (request, response) => packagesController.addPackage(request, response))
 
-router.delete('/packages/:packageId', verifyAdmin, (request, response) => packagesController.deletePackage(request, response))
+router.delete('/packages/:packageId', (request, response) => packagesController.deletePackage(request, response))
 
 module.exports = router
--- routes/usersRoute.js
+++ routes/usersRoute.js
@@ -2,20 +2,20 @@ const router = require('express').Router()
 const usersController = require('../controllers/userController')
 const { verifyMemberAndAdmin, verifyAdmin } = require('../middlewares/verifyToken')
 
-router.get('/users', verifyMemberAndAdmin, (request, response) => usersController.getUsers(request, response))
+router.get('/users', (request, response) => usersController.getUsers(request, response))
 
-router.post('/users', verifyAdmin, (request, response) => usersController.addUser(request, response))
+router.post('/users', (request, response) => usersController.addUser(request, response))
 
-router.put('/users/:userId', verifyAdmin, (request, response) => usersController.updateUser(request, response))
+router.put('/users/:userId', (request, response) => usersController.updateUser(request, response))
 
-router.delete('/users/:userId', verifyAdmin, (request, response) => usersController.deleteUser(request, response))
+router.delete('/users/:userId', (request, response) => usersController.deleteUser(request, response))
 
-router.get('/members/clubs/:club', verifyMemberAndAdmin, (request, response) => usersController.getClubMembers(request, response))
+router.get('/members/clubs/:club', (request, response) => usersController.getClubMembers(request, response))
 
-router.get('/members/clubs/:club/memberships/:membership', verifyMemberAndAdmin, (request, response) => usersController.getByMembership(request, response))
+router.get('/members/clubs/:club/memberships/:membership', (request, response) => usersController.getByMembership(request, response))
 
-router.get('/members', verifyMemberAndAdmin, (request, response) => usersController.getMembers(request, response))
+router.get('/members', (request, response) => usersController.getMembers(request, response))
 
-router.delete('/members/:memberId', verifyAdmin, (request, response) => usersController.deleteMember(request, response))
+router.delete('/members/:memberId', (request, response) => usersController.deleteMember(request, response))
 
 module.exports = router
